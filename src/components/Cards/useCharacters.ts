dfaaad590e34b11f4d7923cabe333d6845e69d15
Milos Naskovic <milosnaskovic17@gmail.com>
2022-08-30 14:45:39 +0200

    Implemented axios

---
+++ src/components/Cards/useCharacters.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+// Api
+import axios from "../../api/axiosInstance";
+
+// Type
+interface ICharacter {
+  id: number;
+  name: string;
+  status: string;
+  image: string;
+}
+
+export default function useCharacters() {
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const { data: characters } = await axios.get("/character");
+        setCharacters(characters);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [characters]);
+
+  return { characters };
+}
