84f4dd4285a13ae5afa69f106f0f67bae9ec1b14
Milos Naskovic <milosnaskovic17@gmail.com>
2022-09-01 09:37:32 +0200

    Created filter.

--- src/components/Characters/useCharacters.ts
+++ src/components/Characters/useCharacters.ts
@@ -1,18 +1,20 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useInfiniteQuery } from "react-query";
 
 // Api
 import axios from "../../api/axiosInstance";
+import FilterContext from "../Filter/filterContext";
 
-// Function for fetching all data
-const fetchCharacters = ({ pageParam = 1 }) => {
-  return axios.get(`/character/?page=${pageParam}`);
-};
-
-// Custom hook
 export default function useCharacters() {
+  const { filterValue } = useContext(FilterContext);
+
+  // Function for fetching all data
+  const fetchCharacters = ({ pageParam = 1 }) => {
+    return axios.get(`/character/?page=${pageParam}&status=${filterValue}`);
+  };
+
   const { isLoading, data, hasNextPage, fetchNextPage } = useInfiniteQuery(
-    "characters",
+    ["characters", filterValue],
     fetchCharacters,
     {
       getNextPageParam: (_lastPage, pages) => {
@@ -44,7 +46,7 @@ export default function useCharacters() {
     return () => {
       document.removeEventListener("scroll", onScroll);
     };
-  }, [fetchNextPage, hasNextPage]);
+  }, [fetchNextPage, hasNextPage, filterValue]);
 
   return {
     data,
--- src/components/Filter/useFilter.ts
+++ src/components/Filter/useFilter.ts
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 // Context
 import FilterContext from "./filterContext";
