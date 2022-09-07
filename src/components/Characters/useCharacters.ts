7023a659f2e6c7c95ca673faa57e301b777d99bd
Milos Naskovic <milosnaskovic17@gmail.com>
2022-09-07 16:03:21 +0200

    Final changes in useCharacters hook.

--- src/components/Characters/useCharacters.ts
+++ src/components/Characters/useCharacters.ts
@@ -10,32 +10,33 @@ import FilterContext from "../../context/filterContext";
 export default function useCharacters() {
   const { filterValue, searchValue } = useContext(FilterContext);
 
-  const { data, hasNextPage, fetchNextPage, status } = useInfiniteQuery(
-    ["characters", filterValue, searchValue],
-    (queryContext) =>
-      fetchCharacters(queryContext.pageParam, filterValue, searchValue),
-    {
-      getNextPageParam: (_lastPage, allPages) => {
-        if (allPages.length < allPages[0].data.info.pages) {
-          return allPages.length + 1;
-        }
-        return undefined;
-      },
-    }
-  );
+  const { data, hasNextPage, fetchNextPage, status, isFetching } =
+    useInfiniteQuery(
+      ["characters", filterValue, searchValue],
+      (queryContext) =>
+        fetchCharacters(queryContext.pageParam, filterValue, searchValue),
+      {
+        getNextPageParam: (_lastPage, allPages) => {
+          if (allPages.length < allPages[0].data.info.pages) {
+            return allPages.length + 1;
+          }
+          return undefined;
+        },
+      }
+    );
 
   useEffect(() => {
-    let fetching = false;
-
-    const onScroll = async () => {
+    const onScroll = () => {
       if (document.scrollingElement) {
         const { scrollHeight, scrollTop, clientHeight } =
           document.scrollingElement;
 
-        if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
-          fetching = true;
-          if (hasNextPage) fetchNextPage();
-          fetching = false;
+        if (
+          !isFetching &&
+          scrollHeight - scrollTop <= clientHeight * 1.5 &&
+          hasNextPage
+        ) {
+          fetchNextPage();
         }
       }
     };
@@ -45,7 +46,7 @@ export default function useCharacters() {
     return () => {
       document.removeEventListener("scroll", onScroll);
     };
-  }, [fetchNextPage, hasNextPage, filterValue]);
+  }, [fetchNextPage, hasNextPage, isFetching]);
 
   return {
     data,
